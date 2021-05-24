9585ff8c38d7eb128604bb40142d32e439ab9e6e
Juan Felipe Mesa Ocampo <felipemesa14@gmail.com>
2021-05-24 16:56:53 -0500

    Se organiza repositorio.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -111,5 +111,10 @@ $app->router->group([
 ], function ($router) {
     require __DIR__.'/../routes/web.php';
 });
+
+// Agregar al array
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
 return $app;
